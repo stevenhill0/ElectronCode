3eae6dd789d3057bd462fbef8ffd05551fd184b1
Steven Hill <steven@stevenhill.co.za>
2023-02-21 16:10:12 +0200

    Setup icon and click to show app

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -1,13 +1,37 @@
+const path = require('path'); // path is the default helper from Node js: its called the path module: it generates paths regardless what operating system user is on
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
 // Floating reference to the mainWindow
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     webPreferences: { nodeIntegration: true, contextIsolation: false },
+    width: 300,
+    height: 500,
+    // The frame property controls showing the status bar or not. False states to not show
+    frame: false,
+    // resizable property controls whether you can resize the window horizontally or vertically
+    resizable: false,
+    show: false,
   });
 
+  // URL for the BrowserWIndow to look at
   mainWindow.loadURL(`File://${__dirname}/src/index.html`);
+
+  // NOTE: you do NOT need to state @2x modifier at the end of 'windows-icon' because electron will choose whether you need the high res version or not
+  const iconName =
+    process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  // The join function in the path library joins two paths and makes sure they work on any operating system
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+
+  // Tray takes only 1 arg: path to the icon image
+  tray = new Tray(iconPath);
+
+  // Can create event handlers on Tray, same as we can with BrowserWindow
+  tray.on('click', () => {
+    mainWindow.show();
+  });
 });
